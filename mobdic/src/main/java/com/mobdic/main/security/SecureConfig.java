8ffd394063af5fa48b9488c035f5650767e0d786
KTH <pni2396@gmail.com>
2025-03-11 13:37:44 +0900

    test: test service, controller
    - Test Service
    - Test Controller

---
+++ mobdic/src/main/java/com/mobdic/main/security/SecureConfig.java
@@ -0,0 +1,7 @@
+package com.mobdic.main.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecureConfig {
+}
---
+++ mobdic/src/main/java/com/mobdic/main/service/TestService.java
@@ -0,0 +1,9 @@
+package com.mobdic.main.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+}
