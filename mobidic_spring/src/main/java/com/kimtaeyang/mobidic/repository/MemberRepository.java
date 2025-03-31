37848f3499377dcf2ad473f14dde8ba466a6a8a7
KTH <pni2396@gmail.com>
2025-03-31 14:39:46 +0900

    feat: 로그인 관련 Service 구현
    - Member entity 정의
    - 로그인, 회원가입 관련 DTO 정의
    - 로그인, 회원가입 Service 로직 구현

---
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.kimtaeyang.mobidic.repository;
+
+import com.kimtaeyang.mobidic.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+    @Query("select m from Member m")
+    List<Member> findAll();
+    Optional<Member> findById(UUID id);
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByUsername(String username);
+}
