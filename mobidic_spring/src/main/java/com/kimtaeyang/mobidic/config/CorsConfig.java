b1e5eef726f1147ce58a546b424a6f8bd62b586a
KTH <pni2396@gmail.com>
2025-03-31 14:35:17 +0900

    security: Spring security 구성
    - JWT 적용
    - CORS 허용 config
    - login, logout 외 나머지는 로그인 필요

---
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.kimtaeyang.mobidic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:63342")); // 프론트엔드 URL (테스트 시 file:// 사용 가능)
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
