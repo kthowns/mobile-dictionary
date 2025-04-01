34d1f36379b998a4e83d6b13343e67d783a60201
KTH <pni2396@gmail.com>
2025-04-01 16:08:30 +0900

    feat: Login, Join API 구현
    - Controller 구현
    - Service 구현

--- mobidic_spring/src/main/java/com/kimtaeyang/mobidic/service/AuthService.java
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/service/AuthService.java
@@ -37,7 +37,7 @@ public class AuthService {
     public void join(@Valid JoinDto.Request request) {
         Member member = Member.builder()
                 .email(request.getEmail())
-                .username(request.getUsername())
+                .nickname(request.getUsername())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .build();
         memberRepository.save(member);
