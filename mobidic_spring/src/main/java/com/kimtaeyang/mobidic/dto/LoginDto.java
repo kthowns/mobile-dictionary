564ec613249463948552dc7e4477786dbf20a6c1
KTH <pni2396@gmail.com>
2025-04-19 12:39:29 +0900

    fix: email validation 추가
    - 간단한 email validation 추가

--- mobidic_spring/src/main/java/com/kimtaeyang/mobidic/dto/LoginDto.java
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/dto/LoginDto.java
@@ -1,18 +1,24 @@
 package com.kimtaeyang.mobidic.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static com.kimtaeyang.mobidic.code.GeneralResponseCode.BAD_REQUEST;
+
 public class LoginDto {
+    private static final String EMAIL_ERROR_MESSAGE = BAD_REQUEST.getMessage();
+
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
     public static class Request{
         @NotBlank
+        @Email(message = "Invalid email pattern")
         private String email;
         @NotBlank
         private String password;
