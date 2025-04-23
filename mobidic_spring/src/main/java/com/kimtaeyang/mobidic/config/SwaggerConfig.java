44af5539135735326059f1b75a8db45562b69d19
KTH <pni2396@gmail.com>
2025-04-23 15:22:05 +0900

    feat: Swagger 기본 설정
    - Security config은 "dev" profile 사용중일 때만 Swagger uri 활성화
    - 기본 Swagger UI 구성

---
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.kimtaeyang.mobidic.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Mobidic Swagger")
+                .description("모든 REST API")
+                .version("1.0.0");
+    }
+}
