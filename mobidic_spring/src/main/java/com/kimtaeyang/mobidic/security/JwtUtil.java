7aabee53d26bd545d0b987fc6d8cd93a3a8d4fb1
KTH <pni2396@gmail.com>
2025-05-12 15:29:30 +0900

    feat: 유연한 토큰 생성 메서드 구현
    - strategy 패턴 구현하여 람다식으로 토큰 생성 유연하게 가능

--- mobidic_spring/src/main/java/com/kimtaeyang/mobidic/security/JwtUtil.java
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/security/JwtUtil.java
@@ -2,13 +2,16 @@ package com.kimtaeyang.mobidic.security;
 
 import com.kimtaeyang.mobidic.config.JwtProperties;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.UUID;
+import java.util.function.Function;
 
 @Component
 @Slf4j
@@ -32,6 +35,15 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + exp))
                 .signWith(jwtProperties.getSecretKey())
                 .compact();
+    }
+
+    public String generateToken(UUID userId, @NonNull Function<JwtBuilder, JwtBuilder> func) {
+        JwtBuilder builder = Jwts.builder()
+                .subject(userId.toString())
+                .issuedAt(new Date())
+                .signWith(jwtProperties.getSecretKey());
+
+        return func.apply(builder).compact();
     }
 
     public boolean validateToken(String token) {
