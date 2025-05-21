662c9df60f0bd169dd399c3e013d41c6b6481ddd
KTH <pni2396@gmail.com>
2025-05-22 01:21:30 +0900

    fix: 퀴즈 처리 방식 수정
    - JWT 토큰 대신 암호화된 토큰 사용
    - 채점 시 결과에 따라 단어의 통계 정보 반영

--- mobidic_spring/src/main/java/com/kimtaeyang/mobidic/repository/RateRepository.java
+++ mobidic_spring/src/main/java/com/kimtaeyang/mobidic/repository/RateRepository.java
@@ -1,9 +1,11 @@
 package com.kimtaeyang.mobidic.repository;
 
 import com.kimtaeyang.mobidic.entity.Rate;
+import com.kimtaeyang.mobidic.entity.Vocab;
 import com.kimtaeyang.mobidic.entity.Word;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -13,8 +15,20 @@ public interface RateRepository extends JpaRepository<Rate, UUID> {
     @Query("select (1.0*sum(r.isLearned)) / count(w)"+
             " from Word w join Rate r"+
             " on w = r.word"+
-            " where w.vocab.id = :vocabId")
-    Optional<Double> getVocabLearningRate(@Param("vocab") UUID vocabId);
+            " where w.vocab = :vocab")
+    Optional<Double> getVocabLearningRate(@Param("vocab") Vocab vocab);
 
     Optional<Rate> findRateByWord(Word word);
+
+    @Modifying
+    @Query("update Rate r " +
+            " set r.correctCount = r.correctCount + 1" +
+            " where r.word = :word")
+    void increaseCorrectCount(@Param("word") Word word);
+
+    @Modifying
+    @Query("update Rate r " +
+            " set r.incorrectCount = r.incorrectCount + 1" +
+            " where r.word = :word")
+    void increaseIncorrectCount(@Param("word") Word word);
 }
