e50c6d9cddb7f3b6a7d7bf287ce54dc605a4396b
KTH <pni2396@gmail.com>
2025-05-23 15:53:55 +0900

    feat: api response dto 정의
    - GeneralResponseDto, ErrorResponseDto 정의

---
+++ mobidic_flutter/lib/dto/ApiResponseDto.dart
@@ -0,0 +1,25 @@
+class GeneralResponseDto {
+  final int status;
+  final String message;
+  final dynamic data;
+
+  GeneralResponseDto({required this.status, required this.message, required this.data});
+
+  factory GeneralResponseDto.fromJson(Map<String, dynamic> json) => GeneralResponseDto(
+      status: json['status'],
+      message: json['message'],
+      data: json['data']);
+}
+
+class ErrorResponseDto {
+  final int status;
+  final String message;
+  final List<Map<String, String>> errors;
+
+  ErrorResponseDto({required this.status, required this.message, required this.errors});
+
+  factory ErrorResponseDto.fromJson(Map<String, dynamic> json) => ErrorResponseDto(
+      status: json['status'],
+      message: json['message'],
+      errors: json['errors']);
+}
