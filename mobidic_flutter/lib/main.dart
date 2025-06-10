10e5c541f7c6048e67f08066dca456aced83156b
KTH <pni2396@gmail.com>
2025-06-10 16:50:05 +0900

    refactor: provider 코드 분리
    - 가독성 향상 및 유지보수 목적

--- mobidic_flutter/lib/main.dart
+++ mobidic_flutter/lib/main.dart
@@ -1,83 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:mobidic_flutter/data/secure_storage_data_source.dart';
+import 'package:mobidic_flutter/providers.dart';
 import 'package:mobidic_flutter/repository/auth_repository.dart';
-import 'package:mobidic_flutter/repository/member_repository.dart';
-import 'package:mobidic_flutter/repository/pronunciation_repository.dart';
 import 'package:mobidic_flutter/repository/rate_repository.dart';
 import 'package:mobidic_flutter/repository/vocab_repository.dart';
-import 'package:mobidic_flutter/repository/word_repository.dart';
 import 'package:mobidic_flutter/view/auth/join_page.dart';
 import 'package:mobidic_flutter/view/auth/log_in_page.dart';
 import 'package:mobidic_flutter/view/learning/phonics_page.dart';
 import 'package:mobidic_flutter/view/list/vocab_list_page.dart';
-import 'package:mobidic_flutter/viewmodel/auth_view_model.dart';
 import 'package:mobidic_flutter/viewmodel/join_view_model.dart';
 import 'package:mobidic_flutter/viewmodel/vocab_view_model.dart';
 import 'package:provider/provider.dart';
 
-import 'data/api_client.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  const String apiBaseUrl = 'http://www.mobidic.shop/api';
 
-  runApp(
-    MultiProvider(
-      providers: [
-        Provider(create: (_) => ApiClient(apiBaseUrl)),
-        Provider(create: (_) => SecureStorageDataSource()),
-        Provider(
-          create:
-              (context) => AuthRepository(
-                context.read<SecureStorageDataSource>(),
-                context.read<ApiClient>(),
-              ),
-        ),
-        Provider(
-          create:
-              (context) => MemberRepository(
-                context.read<ApiClient>(),
-                context.read<AuthRepository>(),
-              ),
-        ),
-        Provider(
-          create:
-              (context) => VocabRepository(
-                context.read<ApiClient>(),
-                context.read<AuthRepository>(),
-              ),
-        ),
-        Provider(
-          create:
-              (context) => WordRepository(
-                context.read<ApiClient>(),
-                context.read<AuthRepository>(),
-              ),
-        ),
-        Provider(
-          create:
-              (context) => RateRepository(
-                context.read<ApiClient>(),
-                context.read<AuthRepository>(),
-              ),
-        ),Provider(
-          create:
-              (context) => PronunciationRepository(
-            context.read<ApiClient>(),
-            context.read<AuthRepository>(),
-          ),
-        ),
-        ChangeNotifierProvider(
-          create:
-              (context) => AuthViewModel(
-                context.read<AuthRepository>(),
-                context.read<MemberRepository>(),
-              ),
-        ),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(MultiProvider(providers: providers, child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -106,8 +43,7 @@ class MyApp extends StatelessWidget {
                   ),
               child: VocabListPage(),
             ),
-        '/phonics':
-        (context) => PhonicsPage()
+        '/phonics': (context) => PhonicsPage(),
       },
     );
   }
